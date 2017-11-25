d5aec714617f7c9230a3e72ef6e66f84f7df7d8d
David <David@LAPTOP-VSEKM5I2>
2017-11-25 12:29:08 +0000

    COMMIT TERMINADO

---
+++ Boletin10-2/src/boletin10/pkg2/Boletin102.java
@@ -0,0 +1,18 @@
+
+package boletin10.pkg2;
+
+/**
+ *
+ * @author David
+ */
+public class Boletin102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwitchMenu inicio = new SwitchMenu();
+        inicio.menuXogo();
+    }
+    
+}
